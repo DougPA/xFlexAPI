36556f0d0bbe9ad3f111754c814aac2c87b3e132
Douglas Adams <doug@dougs-macmini.local>
2017-05-26 22:06:45 -0400

    Initial Commit

---
+++ xFlexAPI/xFlexAPI.h
@@ -0,0 +1,19 @@
+//
+//  xFlexAPI.h
+//  xFlexAPI
+//
+//  Created by Douglas Adams on 5/26/17.
+//  Copyright © 2017 Douglas Adams. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for xFlexAPI.
+FOUNDATION_EXPORT double xFlexAPIVersionNumber;
+
+//! Project version string for xFlexAPI.
+FOUNDATION_EXPORT const unsigned char xFlexAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xFlexAPI/PublicHeader.h>
+
+
