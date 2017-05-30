449e233885109d54a55431b3a5ddd338a4ee8497
Douglas Adams <doug@Dougs-MacMini.local>
2017-05-30 15:57:16 -0400

    Loggin components

--- xFlexAPI/Sources/Misc/LogEntry.swift
+++ xFlexAPI/Sources/Misc/LogEntry.swift
@@ -13,63 +13,63 @@ import Foundation
 
 public final class LogEntry {
     
-    // ----------------------------------------------------------------------------
-    // MARK: - Public Properties
-    
-    public private(set) var message: String
-    public private(set) var level: MessageLevel
-    public private(set) var source: String
-    
-    public let timeStamp = Date().currentTimeZoneDate()
-    public var levelName: String { return level.name }
-    public var color : NSColor { return level.color }
-    
-    // ----------------------------------------------------------------------------
-    // MARK: - Initialization
-    
-    public init(message: String, level: MessageLevel, source: String) {
-        
-        self.message = message
-        self.level = level
-        self.source = source
-    }
-    
-    // ----------------------------------------------------------------------------
-    // MARK: - Public methods
-    
-    /// Return a description of the Log Entry
-    ///
-    /// - returns: a String
-    ///
-    public func description() -> String {
-        
-        return "\(timeStamp), " + message + ", \(level.name) ," + source
-    }
-    /// Return a String value given a property name
-    ///
-    /// - parameter id: a property name
-    ///
-    /// - returns: the value of the property as a String
-    ///
-    public func valueForId(_ id: String) -> String? {
-        switch id {
-        
-        case "message":
-            return message
-        
-        case "levelName":
-            return levelName
-        
-        case "source":
-            return source
-        
-        case "timeStamp":
-            return timeStamp
-        
-        default:
-            return "Unknown"
-        }
-    }
+//    // ----------------------------------------------------------------------------
+//    // MARK: - Public Properties
+//    
+//    public private(set) var message: String
+//    public private(set) var level: MessageLevel
+//    public private(set) var source: String
+//    
+//    public let timeStamp = Date().currentTimeZoneDate()
+//    public var levelName: String { return level.name }
+//    public var color : NSColor { return level.color }
+//    
+//    // ----------------------------------------------------------------------------
+//    // MARK: - Initialization
+//    
+//    public init(message: String, level: MessageLevel, source: String) {
+//        
+//        self.message = message
+//        self.level = level
+//        self.source = source
+//    }
+//    
+//    // ----------------------------------------------------------------------------
+//    // MARK: - Public methods
+//    
+//    /// Return a description of the Log Entry
+//    ///
+//    /// - returns: a String
+//    ///
+//    public func description() -> String {
+//        
+//        return "\(timeStamp), " + message + ", \(level.name) ," + source
+//    }
+//    /// Return a String value given a property name
+//    ///
+//    /// - parameter id: a property name
+//    ///
+//    /// - returns: the value of the property as a String
+//    ///
+//    public func valueForId(_ id: String) -> String? {
+//        switch id {
+//        
+//        case "message":
+//            return message
+//        
+//        case "levelName":
+//            return levelName
+//        
+//        case "source":
+//            return source
+//        
+//        case "timeStamp":
+//            return timeStamp
+//        
+//        default:
+//            return "Unknown"
+//        }
+//    }
     
 }
 
--- xFlexAPI/Sources/Misc/MessageLevel.swift
+++ xFlexAPI/Sources/Misc/MessageLevel.swift
@@ -8,146 +8,142 @@
 
 import Foundation
 
-// ----------------------------------------------------------------------------
-// MARK: - MessageLevel enum
-// ----------------------------------------------------------------------------
-
-// ----------------------------------------------------------------------------
-// MARK: - Comparable definition
-
-public func <(lhs: MessageLevel, rhs: MessageLevel) -> Bool {
-    
-    return lhs.rawValue < rhs.rawValue
-}
-
-// ----------------------------------------------------------------------------
-// MARK: - Equatable definition
-
-public func ==(lhs: MessageLevel, rhs: MessageLevel) -> Bool {
-    
-    return lhs.rawValue == rhs.rawValue
-}
-
-// ----------------------------------------------------------------------------
-// MARK: - MessageLevel Enum
-
-public enum MessageLevel: Int, Comparable {
-    
-    /// MessageLevel values.
-    ///      Changes to this list must be reflected below in allCases, color & name
-    case token = -3, debug = -2, verbose = -1, info = 0, warning, error, fatal
-    
-    // ----------------------------------------------------------------------------
-    // MARK: - Static methods
-    
-    /// Return an array of all MessageLevels
-    ///
-    static let allCases: [MessageLevel] = [.token, .debug, .verbose, .info, .warning, .error, .fatal]
-    
-    /// Return an array of MessageLevel values
-    ///
-    /// - returns: an array of all MessageLevels
-    ///
-    public static func values() -> [Int] {
-        var values = [Int]()
-        
-        for messageLevel in allCases {
-            values.append(messageLevel.rawValue)
-        }
-        return values
-    }
-    /// Return an array of MessageLevel names
-    ///
-    /// - returns: an array of the names of all MessageLevels
-    ///
-    public static func names() -> [String] {
-        var names = [String]()
-        
-        for messageLevel in allCases {
-            names.append(messageLevel.name)
-        }
-        return names
-    }
-    /// Return the MessageLevel of a Flex Command response
-    ///
-    /// - Parameter response: the response as a hex String
-    /// - Returns: the equivalent MessageLevel
-    ///
-    public static func from(_ response: String) -> MessageLevel {
-        var value = MessageLevel.verbose            // "1" is converted to .verbose
-        
-        // is the response "informational"
-        if response.characters.first != "1" {
-            
-            // NO, convert the hex String to an Int
-            let number = Int(response, radix: 16) ?? 0
-            
-            // mask out the error status (bits 24-25) & slide right
-            let bitValue =  ( number & 0x03000000 ) >> 24
-            
-            // convert to a Message Level
-            value = MessageLevel(rawValue: bitValue)!
-        }
-        
-        return value
-    }
-    
-    // ----------------------------------------------------------------------------
-    // MARK: - Public properties
-    
-    /// Return a Color for the current MessageLevel
-    ///
-    public var color: NSColor {
-        // arbitrary color assignments
-        switch self {
-            
-        case .fatal:    // Red
-            return NSColor(srgbRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.8)
-            
-        case .error:    // Red
-            return NSColor(srgbRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.4)
-            
-        case .warning:  // Yellow
-            return NSColor(srgbRed: 1.0, green: 1.0, blue: 0.0, alpha: 0.3)
-            
-        case .info:     // Green
-            return NSColor(srgbRed: 0.0, green: 1.0, blue: 0.0, alpha: 0.5)
-            
-        case .verbose:  // Light Green
-            return NSColor(srgbRed: 0.0, green: 1.0, blue: 0.0, alpha: 0.2)
-            
-        case .debug:    // Dark Black
-            return NSColor(srgbRed: 0.0, green: 0.0, blue: 0.0, alpha: 0.2)
-            
-        case .token:    // Light Blue
-            return NSColor(srgbRed: 0.0, green: 0.0, blue: 1.0, alpha: 0.2)
-        }
-    }
-    /// Return a Name for the current MessageLevel
-    ///
-    public var name: String {
-        switch self {
-            
-        case .token:
-            return "Token"
-            
-        case .debug:
-            return "Debug"
-            
-        case .verbose:
-            return "Verbose"
-            
-        case .info:
-            return "Info"
-            
-        case .warning:
-            return "Warning"
-            
-        case .error:
-            return "Error"
-            
-        case .fatal:
-            return "Fatal"
-        }
-    }
-}
+//// ----------------------------------------------------------------------------
+//// MARK: - Comparable definition
+//
+//public func <(lhs: MessageLevel, rhs: MessageLevel) -> Bool {
+//    
+//    return lhs.rawValue < rhs.rawValue
+//}
+//
+//// ----------------------------------------------------------------------------
+//// MARK: - Equatable definition
+//
+//public func ==(lhs: MessageLevel, rhs: MessageLevel) -> Bool {
+//    
+//    return lhs.rawValue == rhs.rawValue
+//}
+//
+//// ----------------------------------------------------------------------------
+//// MARK: - MessageLevel Enum
+//
+//public enum MessageLevel: Int, Comparable {
+//    
+//    /// MessageLevel values.
+//    ///      Changes to this list must be reflected below in allCases, color & name
+//    case token = -3, debug = -2, verbose = -1, info = 0, warning, error, fatal
+//    
+//    // ----------------------------------------------------------------------------
+//    // MARK: - Static methods
+//    
+//    /// Return an array of all MessageLevels
+//    ///
+//    static let allCases: [MessageLevel] = [.token, .debug, .verbose, .info, .warning, .error, .fatal]
+//    
+//    /// Return an array of MessageLevel values
+//    ///
+//    /// - returns: an array of all MessageLevels
+//    ///
+//    public static func values() -> [Int] {
+//        var values = [Int]()
+//        
+//        for messageLevel in allCases {
+//            values.append(messageLevel.rawValue)
+//        }
+//        return values
+//    }
+//    /// Return an array of MessageLevel names
+//    ///
+//    /// - returns: an array of the names of all MessageLevels
+//    ///
+//    public static func names() -> [String] {
+//        var names = [String]()
+//        
+//        for messageLevel in allCases {
+//            names.append(messageLevel.name)
+//        }
+//        return names
+//    }
+//    /// Return the MessageLevel of a Flex Command response
+//    ///
+//    /// - Parameter response: the response as a hex String
+//    /// - Returns: the equivalent MessageLevel
+//    ///
+//    public static func from(_ response: String) -> MessageLevel {
+//        var value = MessageLevel.verbose            // "1" is converted to .verbose
+//        
+//        // is the response "informational"
+//        if response.characters.first != "1" {
+//            
+//            // NO, convert the hex String to an Int
+//            let number = Int(response, radix: 16) ?? 0
+//            
+//            // mask out the error status (bits 24-25) & slide right
+//            let bitValue =  ( number & 0x03000000 ) >> 24
+//            
+//            // convert to a Message Level
+//            value = MessageLevel(rawValue: bitValue)!
+//        }
+//        
+//        return value
+//    }
+//    
+//    // ----------------------------------------------------------------------------
+//    // MARK: - Public properties
+//    
+//    /// Return a Color for the current MessageLevel
+//    ///
+//    public var color: NSColor {
+//        // arbitrary color assignments
+//        switch self {
+//            
+//        case .fatal:    // Red
+//            return NSColor(srgbRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.8)
+//            
+//        case .error:    // Red
+//            return NSColor(srgbRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.4)
+//            
+//        case .warning:  // Yellow
+//            return NSColor(srgbRed: 1.0, green: 1.0, blue: 0.0, alpha: 0.3)
+//            
+//        case .info:     // Green
+//            return NSColor(srgbRed: 0.0, green: 1.0, blue: 0.0, alpha: 0.5)
+//            
+//        case .verbose:  // Light Green
+//            return NSColor(srgbRed: 0.0, green: 1.0, blue: 0.0, alpha: 0.2)
+//            
+//        case .debug:    // Dark Black
+//            return NSColor(srgbRed: 0.0, green: 0.0, blue: 0.0, alpha: 0.2)
+//            
+//        case .token:    // Light Blue
+//            return NSColor(srgbRed: 0.0, green: 0.0, blue: 1.0, alpha: 0.2)
+//        }
+//    }
+//    /// Return a Name for the current MessageLevel
+//    ///
+//    public var name: String {
+//        switch self {
+//            
+//        case .token:
+//            return "Token"
+//            
+//        case .debug:
+//            return "Debug"
+//            
+//        case .verbose:
+//            return "Verbose"
+//            
+//        case .info:
+//            return "Info"
+//            
+//        case .warning:
+//            return "Warning"
+//            
+//        case .error:
+//            return "Error"
+//            
+//        case .fatal:
+//            return "Fatal"
+//        }
+//    }
+//}
