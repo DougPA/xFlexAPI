397affdce56568fb2cb1a3fa64d8af2fbc82e88d
Douglas Adams <doug@dougs-macmini.local>
2017-06-24 22:02:49 -0400

    Added Xvtr.swift (only a stub, to be completed later)

---
+++ xFlexAPI/Sources/Models/Xvtr.swift
@@ -0,0 +1,38 @@
+//
+//  Xvtr.swift
+//  xFlexAPI
+//
+//  Created by Douglas Adams on 6/24/17.
+//  Copyright © 2017 Douglas Adams. All rights reserved.
+//
+
+import Foundation
+
+// --------------------------------------------------------------------------------
+// MARK: - Waterfall Class implementation
+//
+//      creates an Xvtr instance to be used by a Client to support the
+//      processing of an Xvtr
+//
+// --------------------------------------------------------------------------------
+
+public final class Xvtr : NSObject, KeyValueParser {
+    
+    
+    // TODO: add code
+    
+    
+    // ------------------------------------------------------------------------------
+    // MARK: - KeyValueParser Protocol methods
+    //     called by Radio, executes on the parseQ
+    
+    /// Parse Waterfall key/value pairs
+    ///
+    /// - parameter keyValues: a KeyValuesArray
+    ///
+    public func parseKeyValues(_ keyValues: Radio.KeyValuesArray) {
+        
+        // TODO: add code
+        
+    }
+}
